4b088f4b3ddbe8349c0ad326c9886a8b68d654e3
Jake <779624006@qq.com>
2022-07-17 15:43:36 +0800

    实现isReadonly,isReactive 判断嵌套对象

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,5 +1,6 @@
+import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { ReactiveFlags } from "./reactive";
+import { reactive, ReactiveFlags, readonly } from "./reactive";
 
 export let get = createGetter()
 export let set = createSetter()
@@ -8,12 +9,15 @@ export let readonlyGet = createGetter(true)
 
 function createGetter(isReadonly: boolean = false) {
   return function (target, key) {
-    if(key === ReactiveFlags.IS_REACTIVE){
+    if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
-    }else if(key ===  ReactiveFlags.IS_READONLY){
+    } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly
     }
     let val = Reflect.get(target, key);
+    if (isObject(val)) {
+      return isReadonly ? readonly(val) : reactive(val)
+    }
     if (!isReadonly) {
       track(target, key)
     }
@@ -21,21 +25,21 @@ function createGetter(isReadonly: boolean = false) {
   }
 }
 
-function createSetter(){
-  return function (target, key, value){
+function createSetter() {
+  return function (target, key, value) {
     let res = Reflect.set(target, key, value)
     trigger(target, key)
-    return res    
+    return res
   }
 }
 
-export const  mutableHandler = {
+export const mutableHandler = {
   get, set
-} 
+}
 
 export const readonlyHandler = {
   get: readonlyGet,
-  set(target, key, value){
+  set(target, key, value) {
     console.warn(`${target} 为 readonly 对象,不可被改变`)
     return true
   }
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -6,6 +6,7 @@ describe("readonly", () => {
     const wrapped = readonly(original);
     expect(wrapped).not.toBe(original);
     expect(wrapped.foo).toBe(1);
+    expect(isReadonly(wrapped.bar)).toBe(true)
     expect(isReadonly(wrapped)).toBe(true)
   });
 
